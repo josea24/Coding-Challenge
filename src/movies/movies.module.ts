9a93229b45ab2ef0ab997d5f847c017fc0039b46
josea24 <josealena24@gmail.com>
2022-12-14 17:56:45 -0400

    borrando imports movies.module.ts

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,9 +1,5 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './controllers/movies.controller';
-import { MoviesService } from './services/movies.service';
 
 @Module({
-  controllers: [MoviesController],
-  providers: [MoviesService],
 })
 export class MoviesModule {}

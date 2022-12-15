a4053411584038f53939c699a18d72a102ad3905
Jose Navarro <josealena24@gmail.com>
2022-12-15 19:11:22 -0400

    services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import ormconfig from '../ormconfig';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
@@ -15,16 +13,6 @@ import appConfigSchema from './config/app.schema';
       isGlobal: true,
       load: [appConfig],
       validationSchema: appConfigSchema,
-    }),
-
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        ...ormconfig,
-        logging: configService.get<string>('config.database.log') === 'yes',
-        timezone: 'Z',
-      }),
     }),
 
     MoviesModule,
--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,5 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MoviesService } from './services/movies.service';
+import { MoviesController } from './controllers/movies.controller';
 
 @Module({
+  providers: [MoviesService],
+
+  controllers: [MoviesController],
 })
 export class MoviesModule {}
